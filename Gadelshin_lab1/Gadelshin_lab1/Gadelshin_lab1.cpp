41936f1570768a2f1d2782ff5c090effbe911c7b
Gadelshin Rushan <rushang0delshin@gmail.com>
2022-09-28 00:27:56 +0300

    Завершил
    Исправил каждую функцию проверки, исправил тип статуса работы трубы, навел порядок с применением функций

--- Gadelshin_lab1/Gadelshin_lab1/Gadelshin_lab1.cpp
+++ Gadelshin_lab1/Gadelshin_lab1/Gadelshin_lab1.cpp
@@ -4,23 +4,23 @@
 using namespace std;
 
 //Создание структур
-struct Pipe
+struct Pipe 
 {
     float lenght = 0;
     float diam = 0;
-    int status = -1;
+    bool status = false;
 };
 struct CS
 {
     string name = "";
-    float workshops_num = 0, workshops_num_run = 0;
+    int workshops_num = 0, workshops_num_run = 0;
     float efficiency = 0;
 };
 
 //Создание функций
-void p_status(int status_p)
+void p_status(bool status_p)
 {
-    if (status_p == 1) {
+    if (status_p == false) {
         cout << "Status: Pipe in repair\n";
     }
     else {
@@ -30,86 +30,82 @@ void p_status(int status_p)
 
 float checking(float check_var)
 {
-    while (!check_var || check_var <= 0 )
+    while (((cin >> check_var).fail()) || (cin.peek() != '\n') || check_var <=0)
     {
         cout << "Error!\nInput another number!" << endl;
         cin.clear();
         cin.ignore(INT_MAX, '\n');
-        cin >> check_var;
     }
     return check_var;
 }
 
-int checking_status(int check_st)
+int check_menu(int check_var)
 {
-    while ((!check_st) || (check_st > 2) || (check_st < 1))
+    while (((cin >> check_var).fail()) || (cin.peek() != '\n'))
     {
-        cout << "Error!\nInput one of the predefined states:\n 1.In repair\n 2.In work\n ";
+        cout << "\nERROR! Choose one of the options below!\n\n" << endl;
         cin.clear();
         cin.ignore(INT_MAX, '\n');
-        cin >> check_st;
+    }
+    return check_var;
+}
+
+bool checking_status(bool check_st)
+{
+    while (((cin >> check_st).fail()) || (cin.peek() != '\n'))
+    {
+        cout << "Error!\nInput one of the predefined states:\n 0.In repair\n 1.In work\n ";
+        cin.clear();
+        cin.ignore(INT_MAX, '\n');
     }
     return check_st;
 }
 
 float f_efficiency(int work_num_run, int work_num)
 {
-
     return (100 * (float(work_num_run) / float(work_num)));
 }
 
-void menu_choice()
+int check_work_shops(int w)
 {
-    cout << "\nChoose: \n 1.Create pipe  2.Create CS  3.Show all objects" <<
-        "4.Edit Pipe  5.Edit CS  6.Save  7.Load  0.Exit\n";
-}
-
-float check_work_shops(float w)
-{
-    while (!w || w <= 0 || (w - floor(w) != 0))
+    while (((cin >> w).fail()) || (cin.peek() != '\n') || (w < 0))
     {
         cout << "Error!\nInput another number!" << endl;
         cin.clear();
         cin.ignore(INT_MAX, '\n');
-        cin >> w;
     }
     return w;
 }
 
-float check_working(float work_run, float work)
+int check_working(int work_run, int work)
 {
-    while ((work_run > work) || !work_run || !work)
+    while (((cin >> work_run).fail()) || (cin.peek() != '\n') || (work_run < 0) || (work_run>work))
     {
         cout << "Error!\nInput number of working shops not exceeding the total number of shops: \n";
         cin.clear();
         cin.ignore(INT_MAX, '\n');
-        cin >> work_run;
     }
     return work_run;
 }
 
 void create_pipe(Pipe& p) {
     cout << "\n pipe lenght: ";
-    cin >> p.lenght;
     p.lenght = checking(p.lenght);
     cout << "\n pipe diameter: ";
-    cin >> p.diam;
     p.diam = checking(p.diam);
-    cout << "\n Choose pipe status:\n 1.In repair\n 2.In work\n ";
-    cin >> p.status;
+    cout << "\n Choose pipe status:\n 0.In repair\n 1.In work\n ";
     p.status = checking_status(p.status);
     p_status(p.status);
 }
 
 void create_cs(CS& cs) {
     cout << "\nCS Name: ";
-    cin >> cs.name;
+    cin.clear();
+    cin.ignore(INT_MAX, '\n');
+    getline(cin, cs.name);
     cout << "\nNumber of workshops:";
-    cin >> cs.workshops_num;
     cs.workshops_num = check_work_shops(cs.workshops_num);
     cout << "\nWorkshops at work: ";
-    cin >> cs.workshops_num_run;
-    cs.workshops_num_run = check_work_shops(cs.workshops_num_run);
     cs.workshops_num_run = check_working(cs.workshops_num_run, cs.workshops_num);
     cout << "\nEfficiency:";
     cout << f_efficiency(cs.workshops_num_run, cs.workshops_num) << "%\n";
@@ -117,9 +113,10 @@ void create_cs(CS& cs) {
 
 void show_all(const Pipe& p, CS& cs) {
 
-    if (p.lenght != 0 && p.diam != 0 && p.status != -1) {
+    if (p.lenght != 0 && p.diam != 0) {
         p_status(p.status);
         cout << "\nPipe \nLenght: " << p.lenght << "\nDiameter: " << p.diam << endl;
+        p_status(p.status);
     }
     else
         cout << "\nThere is no pipe"<<endl;
@@ -150,78 +147,82 @@ void load_file(Pipe& p, CS& cs) {
     ifstream file_1;
     string line;
     file_1.open("output_info.txt");
-    getline(file_1, line);
-    p.lenght = stof(line);
-    getline(file_1, line);
-    p.diam = stof(line);
-    getline(file_1, line);
-    p.status = stoi(line);
-    getline(file_1, line);
-    cs.name = line;
-    getline(file_1, line);
-    cs.workshops_num = stoi(line);
-    getline(file_1, line);
-    cs.workshops_num_run = stoi(line);
-    getline(file_1, line);
-    cs.efficiency = stof(line);
-    cout << "The data was successfully loaded from the file!";
+    if (file_1.is_open()) 
+    {
+        getline(file_1, line);
+        p.lenght = stof(line);
+        getline(file_1, line);
+        p.diam = stof(line);
+        getline(file_1, line);
+        p.status = stoi(line);
+        getline(file_1, line);
+        cs.name = line;
+        getline(file_1, line);
+        cs.workshops_num = stoi(line);
+        getline(file_1, line);
+        cs.workshops_num_run = stoi(line);
+        getline(file_1, line);
+        cs.efficiency = stof(line);
+        cout << "The data was successfully loaded from the file!";
+    }
+    else 
+        cout << "\nError!Failed to open file!";
 }
 
 
 int main() {
-   Pipe p; CS cs;
+   Pipe P; CS CS;
    int num_option(-1);
     while (num_option) {
-        menu_choice();
-        cin >> num_option;
+        cout << "\nChoose: \n 1.Create pipe  2.Create CS  3.Show all objects " <<
+            " 4.Edit Pipe  5.Edit CS  6.Save  7.Load  8.Exit\n";
+        num_option = check_menu(num_option);
         switch (num_option)     {
-        case 0: 
-            return 0;
-            break;
-        
         case 1: {
-            create_pipe(p);
+            create_pipe(P);
             break;
             }
         case 2: {
-            create_cs(cs);
+            create_cs(CS);
             break;
             }
         case 3: {
-                show_all(p, cs);
+                show_all(P, CS);
                 break;
             }
         case 4: {
-                if (p.status == -1)
+                if (P.lenght == 0 || P.diam == 0)
                     cout << "\nThere is no pipe to change!" << endl;
-                else if (p.status == 1 || p.status == 2) {
+                else  {
                     cout << "\nInput pipe status: \n";
-                    cin >> p.status;
-                    checking_status(p.status);
-                    cout << "\nNew Pipe status: " << p.status;
-                    cout << "\nChanges accepted!";
+                    P.status = checking_status(P.status);
+                    cout << "\nNew Pipe status: " << P.status;
+                    cout << "\nChanges accepted!" << endl;
                 }
                 break;
             }
         case 5: {
-                if (cs.workshops_num == 0)
+                if (CS.workshops_num == 0)
                     cout << "\nThere is no CS to change!" << endl;
                 else {
                     cout << "\nNew number of workshops in work: \n";
-                    cin >> cs.workshops_num_run;
-                    cs.workshops_num_run = check_work_shops(cs.workshops_num_run);
-                    check_working(cs.workshops_num_run, cs.workshops_num);
+                    CS.workshops_num_run = check_work_shops(CS.workshops_num_run);
+                    check_working(CS.workshops_num_run, CS.workshops_num);
                 }
                 break;
             }
         case 6: {
-                save_file(p, cs);
+                save_file(P, CS);
                 break;
             }
         case 7: {
-                load_file(p, cs);
+                load_file(P, CS);
                 break;
             }
+        case 0: {
+            return 0;
+            break;
+        }
         default: {
             cout << "\nERROR! Choose one of the options below!\n\n";
             break;
